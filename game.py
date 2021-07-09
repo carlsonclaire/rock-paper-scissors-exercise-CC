09c54fb18091ea1682a55fe3d8579a9f61931f65
carlsonclaire <cec9702@stern.nyu.edu>
2021-07-08 20:48:36 -0400

    Validation!

--- game.py
+++ game.py
@@ -5,10 +5,15 @@ print("Rock, Paper, Scissors, Shoot!")
 # ASK FOR A USER INPUT
 # source: add URL here
 x=input("Please choose 'rock', 'paper', 'scissors'")
-
-# VALIDATE THE USER INPUT
 print(x)
 
+# VALIDATE THE USER INPUT
+if (x == "rock") or (x == "paper") or (x == "scissors"):
+    print("valid")
+else:
+    print("OOPS, INVALID, PLEASE TRY AGAIN")
+    exit()
+print("LATER MESSAGES")
 # GENERATE A COMPUTER CHOICE
 
 
