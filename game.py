2cfafee724a152976a8493b70316947f942b1d56
carlsonclaire <cec9702@stern.nyu.edu>
2021-07-08 20:37:09 -0400

    Asked for user input and validated user input

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
 print("Rock, Paper, Scissors, Shoot!")
+
+
+
+# ASK FOR A USER INPUT
+# source: add URL here
+x=input("Please choose 'rock', 'paper', 'scissors'")
+
+# VALIDATE THE USER INPUT
+print(x)
+
+# GENERATE A COMPUTER CHOICE
+
+
+
+# DETERMINE THE WINNER
+
+
+# DISPLAY THE FINAL RESULTS
