14a1d388f6682de3bfedc317519c53b347376a4c
carlsonclaire <cec9702@stern.nyu.edu>
2021-07-13 10:42:25 -0400

    Confirming computer choice

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
-print("Rock, Paper, Scissors, Shoot!")
+
+#IMPORTS
+import random
 
 
+# WHAT WE'RE TRYING TO DO
+print('Rock, Paper, Scissors, Shoot!')
 
 # ASK FOR A USER INPUT
 # source: add URL here
@@ -8,15 +12,19 @@ x=input("Please choose 'rock', 'paper', 'scissors'")
 print(x)
 
 # VALIDATE THE USER INPUT
-if (x == "rock") or (x == "paper") or (x == "scissors"):
-    print("valid")
+# Reminder to ask Prof why we need to print "valid"
+if (x == 'rock') or (x == 'paper') or (x == 'scissors'):
+    print('valid')
 else:
-    print("OOPS, INVALID, PLEASE TRY AGAIN")
+    print('OOPS, INVALID, PLEASE TRY AGAIN')
     exit()
-print("LATER MESSAGES")
+print("USER CHOSE: ",x)
+
 # GENERATE A COMPUTER CHOICE
-
-
+# SOURCE: STACK OVERFLOW
+valid_options = ['rock','paper','scissors']
+c = random.choice(valid_options)
+print("COMPUTER CHOSE:",c)
 
 # DETERMINE THE WINNER
 
